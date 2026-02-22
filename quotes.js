9a82f4cf9ad7635eb0d0b632217b7653fb08cc2e
cpesolarariumc <cpe.solares.johndaril.c@gmail.com>
2026-02-22 19:21:09 +0800

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,23 @@
+// This file contains the list of quotes for the class quotes feature. You can add, remove, or modify quotes as needed.
+const classQuotes = [
+    {
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "Innovation distinguishes between a leader and a follower.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "Stay hungry, stay foolish.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        text: "It does not matter how slowly you go as long as you do not stop.",
+        author: "Confucius"
+    }
+];
